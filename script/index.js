6d27e67ca2d10d104c1d6ccf0b70d9cc6cb7c57e
Nellylie <marie.beka01@gmail.com>
2023-05-21 15:00:12 +0200

    feat(style): update du style

--- script/index.js
+++ script/index.js
@@ -10,6 +10,16 @@ let listAllIngredient = [] // liste des ingredients en tag
 let listAllAppliance = [] // liste des appareils en tag
 let listAllUstensil = [] // liste des ustensiles en tag
 
+// constantes globales pour acceder aux elements du DOM utilisés
+const globalSearch = document.querySelector('.inputSearch')
+const globalSearchButton = document.querySelector('.icone')
+const inputTagIngredient = document.querySelector('[name = "ingredient-name"]')
+const inputTagAppliance = document.querySelector('[name = "appliance-name"]')
+const inputTagUstensil = document.querySelector('[name = "ustensil-name"]')
+const idIconeIngredients = document.querySelector('#i-0')
+const idIconeAppliances = document.querySelector('#i-1')
+const idIconeUstensiles = document.querySelector('#i-2')
+
 // initialise le tableau dynamique des recettes, utile pour lancer des le chargement de la page la construction
 // des recettes
 function fullArrayRecipes () {
@@ -22,12 +32,6 @@ fullArrayRecipes()
 displayRecipesDom()
 // concerne les interactions utilisateurs avec l'interface
 function interactionSearchGlobal () {
-  const globalSearch = document.querySelector('.inputSearch')
-  const globalSearchButton = document.querySelector('.icone')
-  const inputTagIngredient = document.querySelector('[name = "ingredient-name"]')
-  const inputTagAppliance = document.querySelector('[name = "appliance-name"]')
-  const inputTagUstensil = document.querySelector('[name = "ustensil-name"]')
-
   // par défaut au chargement, la barre de recherche n'a pas de mot clé.
   // ici est actualisé le tableau des recettes sans mots clé.
   updateArrayRecipesFiltred('')
@@ -57,6 +61,7 @@ function interactionSearchGlobal () {
   // concerne la liste de tags ingredient à l'entrée d'un caractère de l'utilisateur
   inputTagIngredient.addEventListener('input', (e) => {
     const keyWord = e.target.value
+    inputTagIngredient.style.opacity = '1'
     if (keyWord.length >= 2 || keyWord.length === 0) {
       // actualise l'affichage de la liste selon les premiers caractères entrés par l'utilisateur
       displayTagsDom(keyWord); displayTagClickDom('ingredients') // gère l'affichage de la liste concernée
@@ -65,6 +70,7 @@ function interactionSearchGlobal () {
   // concerne la liste de tags des appareils à l'entrée des caractères de l'utilisateur
   inputTagAppliance.addEventListener('input', (e) => {
     const keyWord = e.target.value
+    inputTagAppliance.style.opacity = '1'
     if (keyWord.length >= 2 || keyWord.length === 0) {
       displayTagsDom(keyWord); displayTagClickDom('appareils')
     }
@@ -72,73 +78,50 @@ function interactionSearchGlobal () {
   // concerne la liste de tags des ustensiles à l'entrée des caractères de l'utilisateur
   inputTagUstensil.addEventListener('input', (e) => {
     const keyWord = e.target.value
+    inputTagUstensil.style.opacity = '1'
     if (keyWord.length >= 2 || keyWord.length === 0) {
       displayTagsDom(keyWord); displayTagClickDom('ustensiles')
     }
   })
 
-  // agit au click de l'utilisateur sur le champs input des ingredients
-  inputTagIngredient.addEventListener('click', (e) => {
-    // recupere la valeur de l'input
-    const keyWord = e.target.value
-    // change l'apparence pour le placeholder
-    inputTagIngredient.placeholder = 'Rechercher un ingrédient'; inputTagIngredient.style.width = '250px'
-    inputTagUstensil.placeholder = 'Ustensiles'; inputTagUstensil.style.width = '100px'
-    inputTagAppliance.placeholder = 'Appareils'; inputTagAppliance.style.width = '100px'
-    // filtre les recettes selon les tags de selectionnés par l'utilisateur
-    filtredRecipesDomByTag()
-    // appel la fonction pour construire les recettes
-    displayRecipesDom()
-    // actualise les tags selon les caractères entrés de l'utilisateur
-    displayTagsDom(keyWord); displayTagClickDom('ingredients')// affiche la liste des tags
-    // gère l'affichage du chevron
-    angleIconeMove('up', '0')
-    angleIconeMove('down', '1')
-    angleIconeMove('down', '2')
-    hideTagClickDom('appareils', '1')
-    hideTagClickDom('ustensiles', '2')
-  })
-  inputTagAppliance.addEventListener('click', (e) => {
-    const keyWord = e.target.value
-    inputTagAppliance.placeholder = 'Rechercher un appareil'; inputTagAppliance.style.width = '250px'
-    inputTagIngredient.placeholder = 'Ingrédients'; inputTagIngredient.style.width = '100px'
-    inputTagUstensil.placeholder = 'Ustensiles'; inputTagUstensil.style.width = '100px'
-    filtredRecipesDomByTag()
-    // appel la fonction pour construire les recettes
-    displayRecipesDom()
-    displayTagsDom(keyWord); displayTagClickDom('appareils')
-    angleIconeMove('up', '1')
-    angleIconeMove('down', '0')
-    angleIconeMove('down', '2')
-    hideTagClickDom('ingredients', '0')
-    hideTagClickDom('ustensiles', '2')
-  })
-  inputTagUstensil.addEventListener('click', (e) => {
-    const keyWord = e.target.value
-    inputTagUstensil.placeholder = 'Rechercher un ustensile'; inputTagUstensil.style.width = '250px'
-    inputTagIngredient.placeholder = 'Ingrédients'; inputTagIngredient.style.width = '100px'
-    inputTagAppliance.placeholder = 'Appareils'; inputTagAppliance.style.width = '100px'
-    filtredRecipesDomByTag()
-    displayTagsDom(keyWord); displayTagClickDom('ustensiles')
-    // appel la fonction pour construire les recettes
-    displayRecipesDom()
-    angleIconeMove('up', '2')
-    angleIconeMove('down', '1')
-    angleIconeMove('down', '0')
-    hideTagClickDom('ingredients', '0')
-    hideTagClickDom('appareils', '1')
+  // agit au click de l'utilisateur sur le bouton et le champs input des ingredients, appareils et ustensiles
+  idIconeIngredients.addEventListener('click', (e) => {
+    openListTagIngredient()
   })
 
+  idIconeAppliances.addEventListener('click', () => {
+    openListTagAppliance()
+  })
+  idIconeUstensiles.addEventListener('click', () => {
+    openListTagUstensiles()
+  })
+  inputTagIngredient.addEventListener('click', () => {
+    openListTagIngredient()
+  })
+
+  inputTagAppliance.addEventListener('click', () => {
+    openListTagAppliance()
+  })
+  inputTagUstensil.addEventListener('click', () => {
+    openListTagUstensiles()
+  })
   // agit au click de l'utilisateur sur le document html
   document.addEventListener('click', (e) => {
+    console.log(e.target)
     // si le click ne concerne pas les tags, ferme les listes de tags
-    if (e.target.className !== 'ingredients-input' && e.target.className !== 'appareils-input' && e.target.className !== 'ustensiles-input' && e.target.className !== 'tag') {
+    if (e.target.className !== 'ingredients-input' && e.target.className !== 'appareils-input' && e.target.className !== 'ustensiles-input' && e.target.className !== 'tag' && e.target.id !== 'i-0' && e.target.id !== 'i-1' && e.target.id !== 'i-2') {
       hideTagClickDom('ingredients', '0')
       hideTagClickDom('appareils', '1')
       hideTagClickDom('ustensiles', '2')
+      angleIconeMove('down', '2')
+      angleIconeMove('down', '1')
+      angleIconeMove('down', '0')
       inputTagUstensil.placeholder = 'Ustensiles'; inputTagUstensil.style.width = '100px'
       inputTagIngredient.placeholder = 'Ingrédients'; inputTagIngredient.style.width = '120px'
       inputTagAppliance.placeholder = 'Appareils'; inputTagAppliance.style.width = '100px'
+      inputTagUstensil.style.opacity = '1'
+      inputTagIngredient.style.opacity = '1'
+      inputTagAppliance.style.opacity = '1'
     }
     // si le click concerne un tag ingredient/appareil/ustensile met à jour les recettes filtré selon les tags de selectionnés
     // ou déselectionnés
@@ -196,18 +179,27 @@ function displayRecipesDom () {
   // le vide
   allRecipes.innerHTML = ''
 
-  // parcourt le tableau des recettes bien filtrés précédemment par les mots clés utilisateur
-  // gràce à la boucle FOR
-  for (let i = 0; i < arrayRecipesFiltred.length; i++) {
+  // verifie si le tableau des recettes filtrées est plein
+  if (arrayRecipesFiltred.length !== 0) {
+    // parcourt le tableau des recettes bien filtrés précédemment par les mots clés utilisateur
+    // gràce à la boucle FOR
+    for (let i = 0; i < arrayRecipesFiltred.length; i++) {
     // destructure dans de nouvelles constantes chacune des recettes
-    const { id, name, ingredients, time, description } = arrayRecipesFiltred[i]
-    const arrayIngredients = []
-    ingredients.forEach((oneIngredient) => {
-      const { ingredient, quantity, unit } = oneIngredient
-      arrayIngredients.push(new IngredientDiv(ingredient, quantity, unit).detailIngredients())
-    })
-    const cardRecipe = new RecipeDiv(id, name, arrayIngredients, time, description).cardsFactory()
-    allRecipes.appendChild(cardRecipe)
+      const { id, name, ingredients, time, description } = arrayRecipesFiltred[i]
+      const arrayIngredients = []
+      ingredients.forEach((oneIngredient) => {
+        const { ingredient, quantity, unit } = oneIngredient
+        arrayIngredients.push(new IngredientDiv(ingredient, quantity, unit).detailIngredients())
+      })
+      const cardRecipe = new RecipeDiv(id, name, arrayIngredients, time, description).cardsFactory()
+      allRecipes.appendChild(cardRecipe)
+    }
+  } else {
+  // si le tableau des recettes filtrées est vide, affiche un message avertissant l'utilisateur
+    const messageDiv = document.createElement('div')
+    messageDiv.setAttribute('class', 'message')
+    messageDiv.textContent = 'Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc...'
+    allRecipes.appendChild(messageDiv)
   }
 }
 
@@ -290,4 +282,64 @@ function displayTagsDom (keyWord) {
   formatOneData(regexWordArrayTagList(keyWord, listAllUstensil)).forEach((ustensil) => {
     listeUstensil.appendChild(tagButton(caseData(ustensil), 'ustensil'))
   })
+}
+
+// rassemble les fonctions dédiées à l'ouverture des tags, la réactualisation de l'affichage de la liste des tags
+// et la réactualisation de l'affichage des recettes
+// utilisées pour chaque bouton spécifique lié à la nature du tag
+function openListTagIngredient () {
+  // recupere la valeur de l'input
+  const keyWord = inputTagIngredient.value
+  // change l'apparence pour le placeholder
+  inputTagIngredient.style.opacity = '0.5'
+  inputTagIngredient.placeholder = 'Rechercher un ingrédient'; inputTagIngredient.style.width = '250px'
+  inputTagUstensil.placeholder = 'Ustensiles'; inputTagUstensil.style.width = '100px'; inputTagUstensil.style.opacity = '1'
+  inputTagAppliance.placeholder = 'Appareils'; inputTagAppliance.style.width = '100px'; inputTagAppliance.style.opacity = '1'
+
+  // filtre les recettes selon les tags de selectionnés par l'utilisateur
+  filtredRecipesDomByTag()
+  // appel la fonction pour construire les recettes
+  displayRecipesDom()
+  // actualise les tags selon les caractères entrés de l'utilisateur
+  displayTagsDom(keyWord); displayTagClickDom('ingredients')// affiche la liste des tags
+  // gère l'affichage du chevron
+  angleIconeMove('up', '0')
+  angleIconeMove('down', '1')
+  angleIconeMove('down', '2')
+  hideTagClickDom('appareils', '1')
+  hideTagClickDom('ustensiles', '2')
+}
+
+function openListTagAppliance () {
+  const keyWord = inputTagAppliance.value
+  inputTagAppliance.style.opacity = '0.5'
+  inputTagAppliance.placeholder = 'Rechercher un appareil'; inputTagAppliance.style.width = '250px'
+  inputTagIngredient.placeholder = 'Ingrédients'; inputTagIngredient.style.width = '100px'; inputTagIngredient.style.opacity = '1'
+  inputTagUstensil.placeholder = 'Ustensiles'; inputTagUstensil.style.width = '100px'; inputTagUstensil.style.opacity = '1'
+  filtredRecipesDomByTag()
+  // appel la fonction pour construire les recettes
+  displayRecipesDom()
+  displayTagsDom(keyWord); displayTagClickDom('appareils')
+  angleIconeMove('up', '1')
+  angleIconeMove('down', '0')
+  angleIconeMove('down', '2')
+  hideTagClickDom('ingredients', '0')
+  hideTagClickDom('ustensiles', '2')
+}
+
+function openListTagUstensiles () {
+  const keyWord = inputTagUstensil.value
+  inputTagUstensil.style.opacity = '0.5'
+  inputTagUstensil.placeholder = 'Rechercher un ustensile'; inputTagUstensil.style.width = '250px'
+  inputTagIngredient.placeholder = 'Ingrédients'; inputTagIngredient.style.width = '100px'; inputTagIngredient.style.opacity = '1'
+  inputTagAppliance.placeholder = 'Appareils'; inputTagAppliance.style.width = '100px'; inputTagAppliance.style.opacity = '1'
+  filtredRecipesDomByTag()
+  displayTagsDom(keyWord); displayTagClickDom('ustensiles')
+  // appel la fonction pour construire les recettes
+  displayRecipesDom()
+  angleIconeMove('up', '2')
+  angleIconeMove('down', '1')
+  angleIconeMove('down', '0')
+  hideTagClickDom('ingredients', '0')
+  hideTagClickDom('appareils', '1')
 }
