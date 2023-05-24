e84e90f200bace4d079a2cc11ac9994da45169d1
Nellylie <marie.beka01@gmail.com>
2023-05-24 22:59:16 +0200

    fix

--- script/index.js
+++ script/index.js
@@ -125,7 +125,7 @@ function interactionSearchGlobal () {
     }
     // si le click concerne un tag ingredient/appareil/ustensile met à jour les recettes filtré selon les tags de selectionnés
     // ou déselectionnés
-    if (e.target.className === 'tag-ingredient tag col-sm-auto' || e.target.className === 'tag-appliance tag col-sm-auto' || e.target.className === 'tag-ustensil tag col-sm-auto' || e.target.classList === 'close') {
+    if (e.target.className === 'tag-ingredient tag col-sm-auto' || e.target.className === 'tag-appliance tag col-sm-auto' || e.target.className === 'tag-ustensil tag col-sm-auto' || e.target.classList.contains('close')) {
       filtredRecipesDomByTag()
       // appel la fonction pour construire les recettes
       displayRecipesDom()
